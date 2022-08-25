bf5d411bc7a0c9d90afd4ffaf94a4719061a901d
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-25 16:40:30 +0530

    added ComponentModule, HttpClientModule, TranslateModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ComponentModule } from './components/component.module';
+import { FilterPipe } from './pipes/filter.pipe';
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+ return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }), 
   ],
   providers: [],
   bootstrap: [AppComponent]
